a45652b1a1d0161488a23dded57b1ab0c8cffbfe
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2021-03-27 09:43:52 -0300

    .

--- GerMonitorias/src/negocio/SistemaMonitoria.java
+++ GerMonitorias/src/negocio/SistemaMonitoria.java
@@ -1,5 +1,27 @@
 package negocio;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import dados.Materia;
+
 public class SistemaMonitoria {
+	private Scanner s = new Scanner(System.in);
+	private List<Materia> materias = new ArrayList<Materia>();
+	
+	/*
+	  criarDiscussao(...)
+	  editarDiscussão(...)
+	  avaliarMonitor(...)
+	  adicionarMateria(...)
+	  excluirMateria(...)
+	  editarMateria(...)
+	  adicionarAluno(...)
+	  adiconarMonitor(...)
+	  adicionarProfessor(...)
+	  adicionarPost(...)
+	  adicionarResposta(...)
+	  */
+	
 	
 }
